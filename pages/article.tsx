e6959f8850b2ecbae07170ada5a6db8741568e4b
Federico Torres <federicofederico881@gmail.com>
2022-04-11 12:00:37 -0300

    refactor: change file extensions js -> ts & tsx

--- pages/article.tsx
+++ pages/article.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import type { NextPage } from 'next';
+
 // Layout de articulo
 export default function article() {
   return (
