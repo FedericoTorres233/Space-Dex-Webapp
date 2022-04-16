74f695edcbee88a56c85bd8f0aab86c945b34444
Federico Torres <federicofederico881@gmail.com>
2022-04-16 16:58:48 -0300

    feat: add tailwindcss to project

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./pages/article/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  corePlugins: {
+    preflight: false,
+  },
+};
