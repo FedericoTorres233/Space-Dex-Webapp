1a962ca5684712f584f479a94ddc2db6525fa7e4
Federico Torres <federicofederico881@gmail.com>
2022-05-07 20:47:45 -0300

    style: use prose class for article content

--- components/ArticleContent.jsx
+++ components/ArticleContent.jsx
@@ -3,12 +3,15 @@ import emoji from "node-emoji";
 
 export default function ArticleContent({ content }) {
   return (
-    <div
-      dangerouslySetInnerHTML={{
-        __html: marked(
-          content.replace(/(:.*:)/g, (match) => emoji.emojify(match))
-        ),
-      }}
-    ></div>
+    <div className="grid place-items-center">
+      <div
+        className="prose prose-lg xl:prose-xl"
+        dangerouslySetInnerHTML={{
+          __html: marked(
+            content.replace(/(:.*:)/g, (match) => emoji.emojify(match))
+          ),
+        }}
+      ></div>
+    </div>
   );
 }
