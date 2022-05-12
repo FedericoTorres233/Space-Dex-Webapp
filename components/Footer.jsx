f11446e10699619d2570fd66029a811899066ccd
Federico Torres <federicofederico881@gmail.com>
2022-05-12 09:00:56 -0300

    refactor: replace custom css with tailwindcss in footer component

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,21 +1,22 @@
 // Footer general
-import classes from "../styles/Footer.module.css";
 import Emoji from "./utils/Emoji";
 
 export default function Footer() {
   return (
-    <footer className={classes.footer}>
+    <footer className="mt-20 grid text-white bg-slate-900 place-items-center min-h-[4rem]">
       <div>
         <a
+          className="hover:text-gray-400 transition-all ease-in-out"
           target="_blank"
           rel="noreferrer"
           href="https://github.com/FedericoTorres233/spacedex"
         >
           <strong>SpaceDex </strong>
-          <Emoji symbol=":comet:"/>{" "}
+          <Emoji symbol=":comet:" />{" "}
         </a>
         - A project by{" "}
         <a
+          className="hover:text-gray-400 transition-all ease-in-out"
           target="_blank"
           rel="noreferrer"
           href="https://github.com/FedericoTorres233"
