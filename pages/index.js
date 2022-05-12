b61594b62114261430f6314266bf7981f0cb97e8
Federico Torres <federicofederico881@gmail.com>
2022-05-12 14:41:50 -0300

    refactor: replace custom css with tailwindcss in index page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import MainPageHeader from "../components/MainPage-Header";
 import MainPageContent from "../components/MainPage-Content";
-import classes from "../styles/Index.module.css";
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
@@ -9,7 +8,7 @@ import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 // Index page
 export default function Home({ articles }) {
   return (
-    <body className={classes.wrapper}>
+    <body className="m-0 p-0 bg-[#15151c] top-0 left-0">
       <Parallax pages={4}>
         <ParallaxLayer offset={0} speed={0.5} factor={1}>
           <video muted autoPlay loop className="object-cover">
@@ -22,7 +21,7 @@ export default function Home({ articles }) {
         </ParallaxLayer>
 
         <ParallaxLayer offset={1} speed={0.5}>
-          <div className={classes.section}>
+          <div className="text-7xl py-8 px-12 bg-[#15151c] text-black">
             <MainPageContent />
           </div>
         </ParallaxLayer>
