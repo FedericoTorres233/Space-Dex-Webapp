69cbd12fb314bb9d3520ee53a98a92a66c117164
Federico Torres <federicofederico881@gmail.com>
2022-05-14 22:53:09 -0300

    refactor: create a component for the discover section content

--- components/mainpage/DiscoverSection.jsx
+++ components/mainpage/DiscoverSection.jsx
@@ -1,65 +1,18 @@
-import SimulationGif from "../../public/UniverseSandboxGIFMain.gif";
-import ISSgifAnimated from "../../public/ISSgifAnimated.gif";
 import Emoji from "../utils/Emoji";
-import Image from "next/image";
 
 export default function DiscoverSection() {
   return (
-    <>
-      <div className="py-28 px-2 grid place-items-center text-[#f0ffff] text-5xl">
-        <h1>
-          <strong>
-            Discover the Universe! <Emoji symbol=":rocket:" />
-          </strong>
-        </h1>
-        <h4 className="text-[#bd5eab] py-3 px-1 text-center">
-          From a single electron to a galaxy, the Universe is complex and full
-          of mysteries. Follow me in this journey and let&apos;s discover every
-          part of it!
-        </h4>
-      </div>
-      <div className="py-20 px-8 bg-[#252736] w-full mb-8 grid place-items-center text-4xl shadow-lg shadow-black">
-        <div className="flex justify-between text-center p-4">
-          <Image
-            height={"650%"}
-            width={"650%"}
-            className="rounded-[50px]"
-            title="ISS trayectory plot"
-            src={ISSgifAnimated}
-            alt="gif"
-            unoptimized={true}
-          />
-          <h4 className="pl-20 grid place-items-center text-[#c38fd3]">
-            Animated gifs, graphs and code examples to learn science{" "}
-            <Emoji symbol=":telescope:" /> while also learning to code{" "}
-            <Emoji symbol=":male-technologist:" />!
-          </h4>
-        </div>
-        <h4 className="text-[#c38fd3]">
-          Simulations using{" "}
-          <a
-            className="text-[#c236aa] hover:text-[#742466]"
-            target="_blank"
-            rel="noreferrer"
-            href="https://universesandbox.com/"
-          >
-            Universe Sandbox 2
-          </a>{" "}
-          <Emoji symbol=":ringed_planet:" /> and videos{" "}
-          <Emoji symbol=":movie_camera:" />
-        </h4>
-        <div>
-          <Image
-            height={"420%"}
-            width={"880%"}
-            src={SimulationGif}
-            className="rounded-[50px]"
-            alt="UniverseSandbox2 - GIF"
-            title="UniverseSandbox2 - GIF"
-            unoptimized={true}
-          />
-        </div>
-      </div>
-    </>
+    <div className="py-28 px-2 grid place-items-center text-[#f0ffff] text-5xl">
+      <h1>
+        <strong>
+          Discover the Universe! <Emoji symbol=":rocket:" />
+        </strong>
+      </h1>
+      <h4 className="text-[#bd5eab] py-3 px-1 text-center">
+        From a single electron to a galaxy, the Universe is complex and full of
+        mysteries. Follow me in this journey and let&apos;s discover every part
+        of it!
+      </h4>
+    </div>
   );
 }
--- components/mainpage/MainPage-Content.jsx
+++ components/mainpage/MainPage-Content.jsx
@@ -1,5 +1,6 @@
 import { ParallaxLayer } from "@react-spring/parallax";
 import Contribute from "./Contribute";
+import DiscoverContent from "./DiscoverContent";
 import DiscoverSection from "./DiscoverSection";
 
 export default function MainPageContent() {
@@ -7,6 +8,10 @@ export default function MainPageContent() {
     <div className="text-7xl py-8 px-12 bg-[#15151c] text-black">
       <ParallaxLayer offset={1} speed={0.2} factor={1.5}>
         <DiscoverSection />
+      </ParallaxLayer>
+
+      <ParallaxLayer offset={2} speed={0.5}>
+        <DiscoverContent />
       </ParallaxLayer>
 
       <ParallaxLayer offset={4} speed={0.5}>
